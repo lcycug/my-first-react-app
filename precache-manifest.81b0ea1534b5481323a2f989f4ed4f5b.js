8e714b0cea1aad5f7c7c70a936d307d9dcdf9d9f
Jason Lee <lcycug@live.com>
2019-01-19 10:36:29 +0800

    Updates

--- precache-manifest.81b0ea1534b5481323a2f989f4ed4f5b.js
+++ precache-manifest.81b0ea1534b5481323a2f989f4ed4f5b.js
@@ -4,20 +4,20 @@ self.__precacheManifest = [
     "url": "/my-first-react-app/static/media/fa-regular-400.8c1f0780.eot"
   },
   {
-    "revision": "0c43aec3ebae771b864d",
+    "revision": "58f63430736f38d5fa6a",
     "url": "/my-first-react-app/static/css/main.d55aec82.chunk.css"
   },
   {
-    "revision": "f6b84cc0c44b8101ab3f",
-    "url": "/my-first-react-app/static/js/1.f6b84cc0.chunk.js"
+    "revision": "279f003febd676f6241c",
+    "url": "/my-first-react-app/static/js/1.279f003f.chunk.js"
   },
   {
     "revision": "47b48b53b14a79423b09",
     "url": "/my-first-react-app/static/js/runtime~main.47b48b53.js"
   },
   {
-    "revision": "eac60e8a656781e13d2a674b4d9051c0",
-    "url": "/my-first-react-app/static/media/fa-brands-400.eac60e8a.woff2"
+    "revision": "d30c44aaad8efa7626428c8294f7e880",
+    "url": "/my-first-react-app/static/media/fa-brands-400.d30c44aa.woff"
   },
   {
     "revision": "cbd387d93e253048800dc9ee22b85c24",
@@ -28,32 +28,28 @@ self.__precacheManifest = [
     "url": "/my-first-react-app/static/media/fa-brands-400.d6ac6c96.ttf"
   },
   {
-    "revision": "d30c44aaad8efa7626428c8294f7e880",
-    "url": "/my-first-react-app/static/media/fa-brands-400.d30c44aa.woff"
-  },
-  {
-    "revision": "e32cb360659f3788d9f4b5750c2c5a36",
-    "url": "/my-first-react-app/static/media/fa-regular-400.e32cb360.woff"
+    "revision": "eac60e8a656781e13d2a674b4d9051c0",
+    "url": "/my-first-react-app/static/media/fa-brands-400.eac60e8a.woff2"
   },
   {
     "revision": "4b218302f9057d02864d4909661831e9",
     "url": "/my-first-react-app/static/media/fa-regular-400.4b218302.woff2"
   },
   {
-    "revision": "0c43aec3ebae771b864d",
-    "url": "/my-first-react-app/static/js/main.0c43aec3.chunk.js"
+    "revision": "e32cb360659f3788d9f4b5750c2c5a36",
+    "url": "/my-first-react-app/static/media/fa-regular-400.e32cb360.woff"
   },
   {
-    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
-    "url": "/my-first-react-app/static/media/fa-brands-400.fef43c1f.svg"
-  },
-  {
-    "revision": "5dc01cfcd5336f696cb85da7ce53fa9b",
-    "url": "/my-first-react-app/static/media/fa-solid-900.5dc01cfc.woff2"
+    "revision": "58f63430736f38d5fa6a",
+    "url": "/my-first-react-app/static/js/main.58f63430.chunk.js"
   },
   {
     "revision": "f1994ecd58b56afa035ae3da39213357",
     "url": "/my-first-react-app/static/media/fa-regular-400.f1994ecd.ttf"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/my-first-react-app/static/media/fa-regular-400.734248d0.svg"
   },
   {
     "revision": "2e302fa4c6eeb1bc06149067bae3e7b4",
@@ -64,8 +60,12 @@ self.__precacheManifest = [
     "url": "/my-first-react-app/static/media/fa-solid-900.a8eedaad.woff"
   },
   {
-    "revision": "734248d027cbaac7f03c0d931ec95593",
-    "url": "/my-first-react-app/static/media/fa-regular-400.734248d0.svg"
+    "revision": "5dc01cfcd5336f696cb85da7ce53fa9b",
+    "url": "/my-first-react-app/static/media/fa-solid-900.5dc01cfc.woff2"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/my-first-react-app/static/media/fa-brands-400.fef43c1f.svg"
   },
   {
     "revision": "80c404ff42e52d9e7589e83fe21307b4",
@@ -76,7 +76,7 @@ self.__precacheManifest = [
     "url": "/my-first-react-app/static/media/fa-solid-900.27603739.svg"
   },
   {
-    "revision": "4efe7b99e884b196cf521a0d5aca22a8",
+    "revision": "d16620cc2b1f6512f2fb196bb42b434b",
     "url": "/my-first-react-app/index.html"
   }
 ];
